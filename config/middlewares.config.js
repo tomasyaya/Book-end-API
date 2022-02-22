8c55997053bec1fd2d9e5d989a2260ffdd62ccc1
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2022-02-22 19:22:54 +0100

    fix static path

--- config/middlewares.config.js
+++ config/middlewares.config.js
@@ -12,7 +12,7 @@ function middlewares(app) {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cors({ credentials: true, origin: ORIGIN }));
-    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname,"..", 'public')));
   } catch (err) {
     console.log("error", err.message);
   }
